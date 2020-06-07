7d8f4fcbad1706398032ecc9f9d6ea73fcfff08a
dogger-work <dogger.work@gmail.com>
2020-06-07 16:29:29 +0300

    Add brands section

--- src/js/parts/sliders.js
+++ src/js/parts/sliders.js
@@ -8,3 +8,31 @@ new Swiper('#home-slider', {
 		prevEl: '.swiper-button-prev',
 	},
 })
+
+new Swiper('#brands__slider', {
+	pagination: {
+		el: '.swiper-pagination',
+	},
+
+	navigation: {
+		nextEl: '.brands__next',
+		prevEl: '.brands__prev',
+	},
+
+	slidesPerView: 2,
+	spaceBetween: 40,
+	breakpoints: {
+		768: {
+			slidesPerView: 4,
+			spaceBetween: 40,
+		},
+		1000: {
+			slidesPerView: 5,
+			spaceBetween: 55,
+		},
+		1600: {
+			slidesPerView: 6,
+			spaceBetween: 90,
+		},
+	}
+})
